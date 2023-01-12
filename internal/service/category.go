c34b2fed480e197eed070657a72f6fad2d63748b
Gabriel Cesario <gcesario203@gmail.com>
2023-01-12 01:26:44 +0000

    feat: implementaçao de listagem de cats

--- internal/service/category.go
+++ internal/service/category.go
@@ -33,3 +33,26 @@ func (c *CategoryService) CreateCategory(ctx context.Context, request *pb.Create
 
 	return categoryResponse, nil
 }
+
+func (c *CategoryService) ListCategories(ctx context.Context, request *pb.Blank) (*pb.CategoryList, error) {
+
+	categories, err := c.CategoryDB.FindAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+
+	for _, category := range categories {
+		categoryResponse := &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		}
+
+		categoriesResponse = append(categoriesResponse, categoryResponse)
+	}
+
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
+}
