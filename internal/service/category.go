0cb822d36b9472b590b22243c5d5573bb6d16333
Gabriel Cesario <gcesario203@gmail.com>
2023-01-12 00:37:23 +0000

    feat: implementacao de interface grpc

---
+++ internal/service/category.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"context"
+
+	"github.com/gcesario203/fullcycle-go-grpc/internal/database"
+	"github.com/gcesario203/fullcycle-go-grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, request *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(request.Name, request.Description)
+
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
